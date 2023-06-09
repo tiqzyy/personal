f15f30e9966180cbc6ed52c14f114a89cc1920b2
Anwar <136066740+tiqzyy@users.noreply.github.com>
2023-06-09 18:08:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ hLinks.forEach(link=>{
 
 // auto typing
 var texts = ["Hi, Welcome To My Website", "Have a Nice Day"]; // Array teks yang ingin ditampilkan
-    var speed = 50; // Kecepatan pengetikan ,ilidetik
+    var speed = 50; // Kecepatan pengetikan (dalam milidetik)
     var typingTextElement = document.getElementById("typing-text");
     var textIndex = 0;
     var charIndex = 0;
@@ -49,3 +49,6 @@ var texts = ["Hi, Welcome To My Website", "Have a Nice Day"]; // Array teks yang
 
     // Memulai pengetikan otomatis ketika halaman dimuat
     window.onload = typeText;
+
+
+
